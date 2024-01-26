79fd0338cf174cb2a0430631fc02ae1a496a5406
Selcuk <selcukemnn@gmail.com>
2024-01-26 11:33:58 +0300

    still needs to changed

--- day01/ex04/Program.java
+++ day01/ex04/Program.java
@@ -10,6 +10,22 @@ public class Program {
 		User user3 = new User("Göksu", 3000);
 		User user4 = new User("Selim", 4000);
 
+		TransactionsService service = new TransactionsService();
+		service.addUser(user1);
+		service.addUser(user2);
+		service.addUser(user3);
+		service.addUser(user4);
+
+		System.out.println("Before:");
+		System.out.println("Selçuk's balance: " + user1.getBalance());
+		System.out.println("Bora's balance: " + user2.getBalance());
+		service.performTransaction(user1.getIdentifier(), user2.getIdentifier(), 500);
+		System.out.println("After:");
+		System.out.println("Selçuk's balance: " + user1.getBalance());
+		System.out.println("Bora's balance: " + user2.getBalance());
+
+
+
 		
 	}
 
