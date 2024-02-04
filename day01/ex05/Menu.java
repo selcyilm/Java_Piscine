c65550d11508c2ebfde05bbb977df879e9760079
Selcuk <selcukemnn@gmail.com>
2024-02-04 23:30:31 +0300

    added menu to programme

--- day01/ex05/Menu.java
+++ day01/ex05/Menu.java
@@ -1,10 +1,33 @@
 package day01.ex05;
 
+import java.util.Scanner;
+
 /**
  * Menu
  */
 public class Menu {
+	private	UsersList			users;
+	private	boolean				isDev;
+	private	TransactionsService	service;
+	private	Scanner				scanner;
 
-	User user;
-	
+	public	Menu(boolean isDev, Scanner scanner) {
+		this.isDev = isDev;
+		this.scanner = scanner;
+		service = new TransactionsService();
+	}
+
+	public	void	printMenu() {
+		int count = 0;
+		System.out.println(++count + ". Add a user");
+		System.out.println(++count + ". View user balances");
+		System.out.println(++count + ". Perform a transfer");
+		System.out.println(++count + ". View all transaction for a spesific user");
+		if (isDev) {
+			System.out.println(++count + ". DEV - remove a transfer by ID");
+			System.out.println(++count + ". DEV - check transfer validity");
+		}
+		System.out.println(++count + ". Finish execution");
+	}
+
 }
--- day01/ex05/Program.java
+++ day01/ex05/Program.java
@@ -1,10 +1,70 @@
 package day01.ex05;
 
+import java.util.Scanner;
+
 /**
  * Program
  */
 public class Program {
-	public static void main(String[] args) {
 
+
+	public	static	int	setSelect(Scanner scanner, int exitInput) {
+		System.out.print("-> ");
+		int setSelect = scanner.nextInt();
+		while (setSelect > exitInput || setSelect < 1) {
+			System.err.print("Invalid Input try again!\n-> ");
+			setSelect = scanner.nextInt();
+		}
+		return (setSelect);
+	}
+
+	public	static	void	goDoTheJob(int input, Scanner scanner, Menu menu) {
+		switch (input) {
+			case 1:
+				
+				break ;
+			case 2:
+
+				break ;
+			case 3:
+
+				break ;
+			case 4:
+
+				break ;
+			case 5:
+
+				break ;
+			case 6:
+
+				break ;
+			default:
+				break ;
+		}
+	}
+
+	public static void main(String[] args) {
+		Menu	menu;
+		Scanner	scanner;
+		int		userInput;
+		int		exitInput;
+		int		select;
+		
+		scanner = new Scanner(System.in);
+		if (args.length != 0 && args[0].equals("--profile=dev")) {
+			exitInput = 7;
+			menu = new Menu(true, scanner);
+		}
+		else {
+			exitInput = 5;
+			menu = new Menu(false, scanner);
+		}
+		userInput = 0;
+		while (userInput != exitInput) {
+		menu.printMenu();
+		userInput = setSelect(scanner, exitInput);
+		System.out.printf("User input is %d\n", userInput);
+		}
+		System.out.println("BYE!");
 	}
 }
