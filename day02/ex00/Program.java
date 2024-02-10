0372af76cd8eefb00b86f4e498958fe2f4ff3c27
Selcuk <selcukemnn@gmail.com>
2024-02-10 22:13:14 +0300

    working in progress

--- day02/ex00/Program.java
+++ day02/ex00/Program.java
@@ -2,27 +2,65 @@ package day02.ex00;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Program
  */
 public class Program {
 	public static void main(String[] args) throws FileNotFoundException {
-	
-		Scanner	scanner = new Scanner(new File("day02/ex00/test.txt"));
-		List<String> lines = new ArrayList<>();
-
+		List<String> hexs = new ArrayList<>();
+		try {
+		FileInputStream fileInputStream = new FileInputStream("day02/ex00/deneme.png");
+		int bt;
+		int	i = 0;
+		while (i < 8) {
+			bt = fileInputStream.read();
+			if (bt != -1) {
+				String str =Integer.toUnsignedString(bt, 16).toUpperCase(); 
+				if (str.length() == 1) {
+					str = "0" + str;
+				}
+				hexs.add(str);
+			}
+			i++;
+		}
+		fileInputStream.close();
+	}
+	catch (IOException e) {
+	e.printStackTrace();
+	}
+		System.out.println(hexs);
+		Map<String, String> signatures = new HashMap<>();
+		File	file = new File("day02/ex00/signatures.txt");
+		String keyValue[] = new String[2];
+		Scanner scanner = new Scanner(file);
 		while (scanner.hasNextLine()) {
-			lines.add(scanner.nextLine());
+			String line = scanner.nextLine();
+			keyValue = line.split(", ");
+			signatures.put(keyValue[0], keyValue[1]);
 		}
-		for (int i = 0; i < lines.size(); i++) {
-			System.out.println(lines.get(i));
+		System.out.println(signatures);
+		String compare = "";
+		for (int i = 0; i < hexs.size(); i++) {
+			compare += hexs.get(i);
+			if (i != hexs.size() - 1)
+				compare += " ";
 		}
-		scanner.close();
-	}	
+		System.out.println(compare);
+		for (Map.Entry<String, String> entry: signatures.entrySet()) {
+			if (entry.getValue().equals(compare))
+				System.out.println(entry.getKey());
+			//System.out.print(entry.getKey() + ": ");
+			//System.out.println(entry.getValue());
+		}
+		//System.out.println("Not found!");
+	}
 }
---
+++ day02/ex00/ft_printf_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf_utils.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: selcyilm <selcyilm@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/19 15:35:17 by selcyilm      #+#    #+#                 */
+/*   Updated: 2024/02/10 22:11:10 by selcyilm      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(const char *s)
+{
+	if (!s)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	write(1, s, ft_strlen(s));
+	return (ft_strlen(s));
+}
+
+int	ft_hex(va_list arg, const char c)
+{
+	unsigned int	hex;
+
+	hex = va_arg(arg, unsigned int);
+	return (ft_putpos(hex, c));
+}
+
+int	ft_putpos(unsigned long int arg, const char c)
+{
+	int	i;
+	int	len;
+
+	len = 0;
+	i = arg % 16;
+	if (arg >= 16)
+		len += ft_putpos(arg / 16, c);
+	if (i < 10)
+		i += 48;
+	else
+	{
+		if (c == 'X')
+			i += 55;
+		else
+			i += 87;
+	}
+	len += write(1, &i, 1);
+	return (len);
+}
